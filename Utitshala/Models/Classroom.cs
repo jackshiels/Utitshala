66a9a0e3a465e4a56196141533dc39908b5fc523
Jack Shiels <SHLJAC006@myuct.ac.za>
2021-08-09 19:14:38 +0200

    Added schools to the domain.

--- Utitshala/Models/Classroom.cs
+++ Utitshala/Models/Classroom.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -13,9 +14,12 @@ namespace Utitshala.Models
     {
         [Key]
         public int ID { get; set; }
+        [ForeignKey("School")]
+        public int? SchoolID { get; set; }
         public string Name { get; set; }
 
         // Virtuals
+        public virtual School School { get; set; }
         public virtual List<Student> Students { get; set; }
         public virtual List<LearningDesign> LearningDesigns { get; set; }
         public virtual List<Assessment> Assessments { get; set; }
---
+++ Utitshala/Models/School.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Utitshala.Models
+{
+    /// <summary>
+    /// A class representing the school that a teacher administrates.
+    /// </summary>
+    public class School
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        // Virtuals
+        public virtual List<Classroom> Classrooms { get; set; }
+    }
+}
